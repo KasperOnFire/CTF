15fb19bba6cf40da8a0ba1152aafed26bc02626d
Kasper Ravn Breindal <kasperbreindal@gmail.com>
2019-08-14 11:38:03 +0200

    redpwn

---
+++ RedpwnCTF2019/redpwnbamboozle/code.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import numpy as np
+import time
+
+data_file = "data2d.txt"
+
+w, h = 600, 800
+data3d = np.zeros((h, w, 3), dtype=np.uint8)
+data2d = np.zeros((h*w, 3), dtype=np.uint8)
+data1d = []
+
+with open(data_file, "r") as in_file:
+    data_in = list(in_file.readlines())
+
+for in_list in data_in[0].strip().split(', '):
+    new_list = list(in_list[1:-1].split(" "))
+    data1d.append(new_list)
+   
+np2d = np.array(data1d).reshape(480000, 3)
+data3d = np2d.reshape(w, h, 3)
+
+img = Image.fromarray(data3d, "RGB")
+img.save("redpwn.jpeg")
+img.show()
